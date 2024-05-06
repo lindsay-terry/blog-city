890b25d2f3ed9ff5e228a094d910680ab3e5274b
Lindsay Terry <lindsaytee66@gmail.com>
2024-05-06 13:29:52 -0600

    moved light/dark mode function to logic js and added CSS for both pages to have light and dark modes

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,27 +1,10 @@
 const backButton = document.querySelector('#back-button')
-const themeSwitch = document.querySelector('#switch-theme');
-const body = document.querySelector('body');
-const modeText = document.querySelector('.theme-text');
 const main = document.querySelector('main');
+
 
 //Send user back to home page 
 backButton.addEventListener('click', function(event) {
     window.location.href = './index.html';
-})
-
-//Toggle light/dark mode
-let mode = 'light';
-
-themeSwitch.addEventListener('click', function () {
-    if (mode === 'light') {
-        mode = 'dark';
-        body.setAttribute('class', 'dark');
-        modeText.textContent = "Light Mode";
-    } else {
-        mode = 'light';
-        body.setAttribute('class', 'light');
-        modeText.textContent = "Dark Mode";
-    }
 })
 
 //render blog posts from local storage on screen
--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,3 +1,7 @@
+const body = document.querySelector('body');
+const themeSwitch = document.querySelector('.switch-theme');
+const modeText = document.querySelector('.theme-text');
+const link = document.querySelector('.light-link');
 let totalPosts = JSON.parse(localStorage.getItem('totalPosts')) || [];
 
 function storeBlogPost(blogPost) {
@@ -9,5 +13,19 @@ function getBlogPosts() {
     return JSON.parse(localStorage.getItem('totalPosts')) || [];
 }
 
+//Toggle light/dark mode
+let mode = 'light';
 
-
+themeSwitch.addEventListener('click', function () {
+    if (mode === 'light') {
+        mode = 'dark';
+        body.setAttribute('class', 'dark');
+        link.setAttribute('class', 'dark-link');
+        modeText.textContent = "Light Mode";
+    } else {
+        mode = 'light';
+        body.setAttribute('class', 'light');
+        link.setAttribute('class', 'light-link');
+        modeText.textContent = "Dark Mode";
+    }
+})
